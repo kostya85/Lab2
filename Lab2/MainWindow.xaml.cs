bcad872d4a92878247ef3764334a347b569499f6
Константин Дмитриевич Горбунов <264892@niuitmo.ru>
2020-11-23 15:08:00 +0300

    Добавлена функция, которая анализирует входящий файл на корректность

--- Lab2/MainWindow.xaml.cs
+++ Lab2/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using Microsoft.Win32;
 using System.Reflection;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Data;
+using System.Net;
 
 namespace Lab2
 {
@@ -27,7 +28,7 @@ namespace Lab2
     {
         public void StartWindow()
         {
-            if (!System.IO.File.Exists("data.xlsx"))
+            if (!System.IO.File.Exists(AppDomain.CurrentDomain.BaseDirectory + "data.xlsx"))
             {
                 
                 SearchButton.Visibility = Visibility.Visible;
@@ -42,6 +43,8 @@ namespace Lab2
                 SearchButton.Visibility = Visibility.Collapsed;
                 DownloadButton.Visibility = Visibility.Collapsed;
                 UpdateData.Visibility = Visibility.Visible;
+                if(CorrectFile(AppDomain.CurrentDomain.BaseDirectory + "data.xlsx"))
+                ParsingFile(AppDomain.CurrentDomain.BaseDirectory + "data.xlsx");
             }
         }
         public MainWindow()
@@ -69,51 +72,127 @@ namespace Lab2
                 }
                 else
                 {
-                    ParsingFile(openFileDialog.FileName);
+                    if (CorrectFile(openFileDialog.FileName))
+                    {
+                        File.Copy(openFileDialog.FileName, AppDomain.CurrentDomain.BaseDirectory + "data.xlsx");
+                        StartWindow();
+                        ParsingFile(AppDomain.CurrentDomain.BaseDirectory + "data.xlsx");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выбранный Вами документ не является корректным!", "Ошибка распознавания");
+                    }
                 }
                 
                 
             }
         }
+      private bool CorrectFile(string fileName)//Проверяет файл на корректность (кол-во колонок, в дальнейшем - названия колонок)
+        {
+            
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(fileName, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            try
+            {
+
+                Excel.Worksheet xlWorkSheet;
+                Excel.Range range;
+                List<Bug> l = new List<Bug>();
+                string str;
+                int rCnt;
+                int cCnt;
+                int rw = 0;
+                int cl = 0;
+
+
+                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                range = xlWorkSheet.UsedRange;
+                rw = range.Rows.Count;
+                cl = range.Columns.Count;
+                xlWorkBook.Close(true, fileName, null);
+                xlApp.Quit();
+                if (cl != 10)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+                            
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Ошибка распознавания документа: " + e.Message, "Ошибка распознавания");
+                return false;
+            }
+           
+                
+                
+                
+            
+        }
         private void ParsingFile(string fileName)
         {
-            Excel.Application xlApp;
-            Excel.Workbook xlWorkBook;
-            Excel.Worksheet xlWorkSheet;
-            Excel.Range range;
-            List<Bug> l = new List<Bug>();
-            string str;
-            int rCnt;
-            int cCnt;
-            int rw = 0;
-            int cl = 0;
-
-            xlApp = new Excel.Application();
-            xlWorkBook = xlApp.Workbooks.Open(fileName, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-            range = xlWorkSheet.UsedRange;
-            rw = range.Rows.Count;
-            cl = range.Columns.Count;
-
-
-            for (rCnt = 3; rCnt <= rw; rCnt++)
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(fileName, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            try
             {
-               
-                //for (cCnt = 1; cCnt <= 3; cCnt++)
-                //{
-                //    str = ((range.Cells[rCnt, cCnt] as Excel.Range).Value2).ToString();
-                //    l1.Add(str);
-                //}
-                string id = ((range.Cells[rCnt, 1] as Excel.Range).Value2).ToString();
-                string des = ((range.Cells[rCnt, 2] as Excel.Range).Value2).ToString();
-                string source= ((range.Cells[rCnt, 3] as Excel.Range).Value2).ToString();
-                l.Add(new Bug(id, des, source));
-            }
+                
+                Excel.Worksheet xlWorkSheet;
+                Excel.Range range;
+                List<Bug> l = new List<Bug>();
+                string str;
+                int rCnt;
+                int cCnt;
+                int rw = 0;
+                int cl = 0;
 
-            xlWorkBook.Close(true, null, null);
-            xlApp.Quit();
-            WholeData.ItemsSource = l;
+               
+                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                range = xlWorkSheet.UsedRange;
+                rw = range.Rows.Count;
+                cl = range.Columns.Count;
+
+
+                for (rCnt = 3; rCnt <= rw; rCnt++)
+                {
+
+                    //for (cCnt = 1; cCnt <= 3; cCnt++)
+                    //{
+                    //    str = ((range.Cells[rCnt, cCnt] as Excel.Range).Value2).ToString();
+                    //    l1.Add(str);
+                    //}
+                    string id = ((range.Cells[rCnt, 1] as Excel.Range).Value2).ToString();
+                    string des = ((range.Cells[rCnt, 2] as Excel.Range).Value2).ToString();
+                    string source = ((range.Cells[rCnt, 3] as Excel.Range).Value2).ToString();
+                    l.Add(new Bug(id, des, source));
+                }
+
+               
+                WholeData.ItemsSource = l;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Ошибка парсинга: "+e.Message);
+            }
+            finally
+            {
+                xlWorkBook.Close(true, fileName, null);
+                xlApp.Quit();
+            }
+        }
+
+        private void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://bdu.fstec.ru/files/documents/thrlist.xlsx";
+            WebClient wc = new WebClient();
+            wc.DownloadFile(url, AppDomain.CurrentDomain.BaseDirectory + "data.xlsx");
+            if (CorrectFile(AppDomain.CurrentDomain.BaseDirectory + "data.xlsx")) ParsingFile(AppDomain.CurrentDomain.BaseDirectory + "data.xlsx");
+            StartWindow();
         }
     }
 }
